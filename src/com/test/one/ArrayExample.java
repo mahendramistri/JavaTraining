36fc4f6720ab23878fd8ddbbc504ff8ff16565ca
mahendra <mahendramistri@gmail.com>
2022-03-10 12:09:48 +0530

    Arrays added

---
+++ src/com/test/one/ArrayExample.java
@@ -0,0 +1,14 @@
+package com.test.one;
+
+public class ArrayExample {
+
+	public static void main(String[] args) {
+		int[] numbers = { 10, 12, 13, 4, 35, 45, 4, 6 };
+
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.print(i);
+		}
+
+	}
+
+}
